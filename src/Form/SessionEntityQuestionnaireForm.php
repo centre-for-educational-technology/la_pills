fac7432225c31d7eccda4d4aa8e918972a1e4fc3
Pjotr Savitski <gnum@tlu.ee>
2019-01-28 13:56:53 +0200

    Fully rewrote the questionnair form build logic.
    New one is no longer using the template but the Form API instead. This
    deals with validation, value preservation and integrats well with form
    data handing logic.

--- src/Form/SessionEntityQuestionnaireForm.php
+++ src/Form/SessionEntityQuestionnaireForm.php
@@ -29,20 +29,54 @@ class SessionEntityQuestionnaireForm extends EntityForm {
 
     $this->questionnaire = $this->entity->getSessionTemplateData()['questionnaires'][$route_match->getParameter('questionnaire_uuid')];
 
-    $form['submit'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Submit'),
-    ];
     $form['questionnaire'] = [
-      '#weight' => '-1',
+      '#markup' => '<h2>' . $this->questionnaire['title'] . '</h2>',
+    ];
+
+    $form['questions'] = [
       '#attached' => [
         'library' => [
           'la_pills/questionnaire'
         ],
       ],
-      '#theme' => 'session_entity_questionnaire',
-      '#session_entity' => $this->entity,
-      '#questionnaire' => $this->questionnaire,
+    ];
+
+    foreach ($this->questionnaire['questions'] as $question) {
+      $structure = [
+        '#title' => $question['title'],
+        '#required' => ($question['required'] === 'Yes') ? TRUE : FALSE
+      ];
+
+      switch($question['type']) {
+        case 'Short text':
+        $structure['#type'] = 'textfield';
+        break;
+        case 'Long text':
+        $structure['#type'] = 'textarea';
+        $structure['#rows'] = 5;
+        break;
+        case 'Scale':
+        $range = range($question['min'], $question['max']);
+        $structure['#type'] = 'radios';
+        $structure['#options'] = array_combine($range, $range);
+        $structure['#attributes']['class'] = ['scale'];
+        break;
+        case 'Multi-choice':
+        $structure['#type'] = 'radios';
+        $structure['#options'] = array_combine($question['options'], $question['options']);
+        break;
+        case 'Checkboxes':
+        $structure['#type'] = 'checkboxes';
+        $structure['#options'] = array_combine($question['options'], $question['options']);
+        break;
+      }
+
+      $form['questions'][$question['uuid']] = $structure;
+    }
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
     ];
 
     return $form;
